1d59995140e4efe70627bd63379f7a89c0650336
Olav Ausland Onstad <olavausland@hotmail.com>
2023-11-21 23:31:44 +0100

    Pushed all dependencies

---
+++ DatabaseInterface/Metadata.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DatabaseInterface
+{
+    [Table("METADATA")]
+    public class Metadata
+    {
+        [Key]
+        public Guid MetadataId { get; set; }
+        [Required]
+        public String Version { get; set; }
+    }
+}
