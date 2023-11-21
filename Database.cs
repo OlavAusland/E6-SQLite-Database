bc7e08ba2bceee4cd849c7415d1c35d6405106b4
Olav Ausland Onstad <olavausland@hotmail.com>
2023-11-21 23:28:02 +0100

    first commit

---
+++ Database.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data.SQLite;
+using System.ComponentModel.DataAnnotations;
+using DatabaseInterface;
+
+namespace DatabaseEF
+{
+    class MTContext : DbContext
+    {
+        public MTContext() : base("name=MTContext")
+        {
+            Database.SetInitializer<MTContext>(new MTContextInitializer());
+        }
+
+        public DbSet<Person> Persons { get; set; }
+
+        public DbSet<Metadata> Metadatas { get; set; }
+    }
+
+    class MTContextInitializer : CreateDatabaseIfNotExists<MTContext>
+    {
+        protected override void Seed(MTContext context)
+        {
+            InitializeTables(context);
+            base.Seed(context);
+            return;
+            context.Database.CreateIfNotExists();
+            Console.WriteLine("SEED CREATING DATABASE");
+            Metadata metadata = new Metadata() { MetadataId = Guid.NewGuid(), Version = "2.8.0" };
+            context.Metadatas.Add(metadata);
+            context.Persons.Add(new Person() { Id = Guid.NewGuid(), Name = "Olav" });
+            base.Seed(context);
+        }
+
+        private void InitializeTables(MTContext context)
+        {
+            context.Database.SqlQuery<Person>("CREATE TABLE Person (Id int, Name TEXT)");
+            Console.WriteLine("TEST");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using DatabaseEF;
+using System;
+using System.Data.Common;
+using System.Data.SQLite;
+using System.Linq;
+DbProviderFactories.RegisterFactory("System.Data.SQLite", System.Data.SQLite.SQLiteFactory.Instance);
+
+
+using(var db = new MTContext())
+{
+    db.Database.CreateIfNotExists();
+    db.Persons.Add(new Person() { Id = Guid.NewGuid(), Name = "Karl" });
+    //db.Database.SqlQuery<Person>("CREATE TABLE Person (Id int, Name TEXT)");
+    db.Database.ExecuteSqlCommand("UPDATE PERSON SET Name = 'Test' WHERE Name = 'Karl'");
+    db.Database.ExecuteSqlCommand("CREATE TABLE BOOK (Id int, Name text)");
+    db.SaveChanges();
+}
